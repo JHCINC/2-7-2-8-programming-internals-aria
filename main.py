ae91ab7b71c4803e090c4d3d8ef7685a25c05433
20090AE <20090@jhc.school.nz>
2025-03-28 09:56:56 +1300

    Still not working trying to fix the comparing budget function and the best budget function :(

--- main.py
+++ main.py
@@ -1,19 +1,52 @@
 # A price comparison tool
 import tkinter as tk
 
-global price, grams, budget, unit_price
+global price , grams , budget , unit_price
 
 # An empty list that amends as the user enters the different product info
 list_product = []
+affordable_products = []
 
 # Function that compares the items in the list
+def get_unit_price(product):
+    return product["unit_price"]
+
+
+# Function which shows which is best for the budget
+def best_budget():
+    affordable_products.clear()
+    budget = float(entry_budget.get())
+    for product in list_product:
+        if product["price"] <= budget:
+            affordable_products.append(product)
+            print("aff",affordable_products)
+# Then it compares each product that is in the budget unit price to find the lowest one
+        best_product = affordable_products[0]
+            for product in affordable_products:
+        if product["unit_price"] < best_product["unit_price"]:
+            print("best", best_product)
+            best_product = product
+
 
 # Function that compares to the budget
+def compare_budget():
+    budget = float(entry_budget.get())
+    for product in list_product:
+        print("product", product)
+        price = product["price"]
+        grams = product["grams"]
+        unit_price = product["unit_price"]
+    if  price <= budget:
+        print("This product is within your budget!")
+    else:
+        print("This product is not within your budget.")
+
 
 # Function that adds items to the list
 def add_item():
     # Get the entry info
     price = float(entry_price.get())
+    print(price)
     grams = float(entry_grams.get())
     unit_price = price / grams
 
@@ -68,8 +101,17 @@ btn_calculate_prices = tk.Button(window, text="Calculate Unit Costs", font=("Ari
 # Text box that displays the unit costs
 tbox_unit_price = tk.Text(window, width=5, height=0, state="disabled")
 
+# Text box for the best unit price for budget
+tbox_best_budget = tk.Text(window, width=5, height=0, state="disabled")
+
 # A button that adds a product to the list
 btn_add_product = tk.Button(window, text="Add product", font=("Arial", 13), command=add_item)
+
+# Button that compares to the budget
+btn_compare_budget = tk.Button(window, text="See which products fit your budget.", font=("Arial", 13),
+                               command=compare_budget)
+
+btn_best_budget = tk.Button(window, text="Best for budget.", font=("Arial", 13,), command=best_budget)
 
 # Placing elements on screen
 title_heading.place(x=180, y=10)
@@ -82,5 +124,8 @@ title_grams.place(x=180, y=140)
 btn_calculate_prices.place(x=160, y=200)
 tbox_unit_price.place(x=180, y=230)
 btn_add_product.place(x=180, y=300)
+btn_compare_budget.place(x=180, y=340)
+tbox_best_budget.place(x=130, y=340)
+btn_best_budget.place(x=100, y=300)
 
 tk.mainloop()
